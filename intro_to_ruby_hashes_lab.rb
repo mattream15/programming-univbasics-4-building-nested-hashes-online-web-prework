8c5b000995bfa409bc8a24254cc62f58b81ef2f5
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-08 21:26:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,8 @@ def monopoly_with_second_tier
       },
       {
     :africa => {
-      ""
+      "Ghana",
+      "Nigeria"
     }
   }
   # When you start writing the implementation for this method, copy the Hash
