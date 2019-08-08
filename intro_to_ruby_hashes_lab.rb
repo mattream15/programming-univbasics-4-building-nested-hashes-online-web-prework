8f33f4ffa919d2156390b29b09f2c27d0177e104
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-08 21:51:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,14 +27,14 @@ def monopoly_with_second_tier
 def monopoly_with_third_tier
   countries_and_capitals_of_the_world = {
     :north_america => {
-      "Canada" => {:capital , :climate},
+      "Canada" => {:capital, :capital_climate},
       "USA" => {:capital,
-      :climate}
+      :capital_climate}
       },
       {
     :africa => {
-      "Ghana" => {:capital, :climate},
-      "Nigeria" => {:capital, :climate}
+      "Ghana" => {:capital, :capital_climate},
+      "Nigeria" => {:capital, :capital_climate}
     }
   }
 end
