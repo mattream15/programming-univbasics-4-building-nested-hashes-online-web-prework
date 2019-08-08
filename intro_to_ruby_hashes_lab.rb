07371c1497845de181918d99a03316bc7ec72832
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-08 21:26:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,11 +9,14 @@ end
 
 def monopoly_with_second_tier
   countries_and_capitals_of_the_world = {
-    :north_america => {"Canada"
+    :north_america => {
+      "Canada",
+      "USA"
       },
-      {"USA"
-      }
-    :africa => {}
+      {
+    :africa => {
+      ""
+    }
   }
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
